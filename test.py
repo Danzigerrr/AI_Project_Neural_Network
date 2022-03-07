7de676bae94a743499b8d8620d97f1b70e1e1032
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2022-03-07 13:56:20 +0100

    added blacklist

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import json
 import pandas
+from blacklist import blacklist
 from sklearn.feature_extraction.text import TfidfVectorizer
 vv = TfidfVectorizer() #obiekt do liczenia slow
 with open("s.json",encoding="utf-8") as file:
     data = json.load(file)
 arr = [] #tabela z tekstami
 numOfText = 25 #liczba analizowanych tekstów
-for i in range(0,numOfText):
-    arr.append(data[i]["text"]) #zapelnienie tablicy z tekatmi elementami "text" z Jsona
+for i in range(0,60):
+    st = data[i]["text"]
+    for j in blacklist:
+        st = st.replace(j, " ")
+    arr.append(st)
 vv.fit(arr) #wrzuecnie do obiektu zeby zaczla liczyc
 print(vv.vocabulary_) #drukowanie listy czestosci slow
 v = vv.transform(arr) #zamiana na macierz
