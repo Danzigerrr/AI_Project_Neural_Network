d6c9fe7f51f5b01faad0c96af3618599074c0637
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2022-03-07 14:11:44 +0100

    added labels

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ import json
 import pandas
 from blacklist import blacklist
 from sklearn.feature_extraction.text import TfidfVectorizer
-vv = TfidfVectorizer() #obiekt do liczenia slow
+vv = TfidfVectorizer(stop_words='english') #obiekt do liczenia slow
 with open("s.json",encoding="utf-8") as file:
     data = json.load(file)
 arr = [] #tabela z tekstami
 numOfText = 25 #liczba analizowanych tekstów
+labels = []
 for i in range(0,60):
+    labels.append(data[i]["isAntiVaccine"])
     st = data[i]["text"]
+    st = st.lower()
     for j in blacklist:
         st = st.replace(j, " ")
     arr.append(st)
@@ -19,3 +22,4 @@ v = vv.transform(arr) #zamiana na macierz
 print(v.toarray()[0]) #drukowanie pierwszego wiersza tej macierzy
 df = pandas.DataFrame(data=v.toarray(), columns=vv.get_feature_names()) #konwersja do koncowej tabelki
 print(df)#drukowanie tabeli
+print(labels)
