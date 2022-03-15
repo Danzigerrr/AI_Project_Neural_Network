efad23cd7460df5c4efa4f8d00762424fb1a42b5
Bartosz Strzelecki <bstrzelecki25@gmail.com>
2022-03-15 14:27:30 +0100

    fixed marker overlapping

--- som.py
+++ som.py
@@ -1,6 +1,7 @@
 from minisom import MiniSom #https://github.com/JustGlowing/minisom
 from functions import classify, get_data
 from pylab import bone,pcolor,colorbar, plot, show
+import random
 def som():
     data, labels = get_data()
     som = MiniSom(15, 15, data.shape[1], sigma=5, learning_rate=0.1,
@@ -11,16 +12,16 @@ def som():
     bone()
     pcolor(som.distance_map().T)
     colorbar()
-    markers = ['o', 's']
+    markers = ['o', 'o']
     colors = ['r', 'g']
     for i, x in enumerate(data.toarray()):
         w = som.winner(x)
-        plot(w[0] + 0.5,
-             w[1] + 0.5,
+        plot(w[0] + random.random(),
+             w[1] + random.random(),
              markers[labels[i]],
              markeredgecolor = colors[labels[i]],
-             markerfacecolor = 'None',
-             markersize = 10,
+             markerfacecolor = colors[labels[i]],
+             markersize = 6,
              markeredgewidth = 2)
     show()
 if __name__ == "__main__":
