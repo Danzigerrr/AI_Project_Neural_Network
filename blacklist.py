7a4dd2a349da3e85445cf47b771776cf6202bb3d
BognaLew <100587236+BognaLew@users.noreply.github.com>
2022-03-17 12:41:10 +0100

    added 'days' to blacklist

--- blacklist.py
+++ blacklist.py
@@ -1,5 +1,5 @@
 
 ignored_tokens = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "[", "]", "\\", "/", "(", ")", "-", ",", ".", "“", "”", "'", ";", ":", "’"]
-blacklist = ["th", "aafp", "aap", "zev", "zelenko", "doing", "hasn", "dr", "md", "ama", "hhs", "ve", "did", "nfid", "gacvs", "tts", "aren", "yeadon", "don", "got", "eua", "didn", "wasn", "wouldn", "isn", "june", "just", "does", "iom", "month", "months", "week", "lot"]
+blacklist = ["th", "aafp", "aap", "zev", "zelenko", "doing", "hasn", "dr", "md", "ama", "hhs", "ve", "did", "nfid", "gacvs", "tts", "aren", "yeadon", "don", "got", "eua", "didn", "wasn", "wouldn", "isn", "june", "just", "does", "iom", "month", "months", "week", "days", "lot"]
 tag_list = ["IN", "PRP", "PRP$", "CC", "DT", "VB", "VBN", "VBD", "VBG", "VBP", "VBZ", "WDT", "WP", "WP$", "MD", "TO", "EX", "CD"]
     
